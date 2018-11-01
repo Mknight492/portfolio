cade75b5a7f04e906afeb73dcf6baf3523dacbb5
Mknight492 <39689669+Mknight492@users.noreply.github.com>
2018-11-01 17:28:49 +1300

    added dynamic focusing of the map based on window size

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import { testFunction } from './modules/console'
 import MobileMenu from './modules/mobile-menu'
+import Map from './modules/map'
 
 testFunction()
 const mobileMenu = new MobileMenu()
+const map = new Map()
+
--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -1,43 +1,55 @@
-var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
-    //console.log(document.documentElement.getElementsByTagName('body')[0].clientWidth)
-export function initMap() {
-    let mapCenter ={}
-    
-    if(window.innerWidth >= 740){
-        //screen is tablet size and map center should be to the right of the logo
-        mapCenter.lat = -41.2942303
-        mapCenter.lng = 174.8270504
-    }else{
-        //screen is phone size and map center should be on the logo
-        mapCenter.lat = -41.2942303
-        mapCenter.lng =174.7958889
+// Map will initialise the google map API
+// if the screen size is less than resize parameter it will center the map on the logo/icon
+// if it large than the resize parameter is will focus the center to the right of the map
+// allowing the contact form to sit in front
+function Map (resize = 740) {
+  
+  let oldWidth = 0
+  this.initMap = () => {
+    let mapCenter = {}
+
+    if (window.innerWidth >= resize) {
+      // screen greater than resize and map center should be to the right of the logo
+      mapCenter.lat = -41.2942303
+      mapCenter.lng = 174.8270504
+    } else {
+      // screen is less than resize and map center should be on the logo
+      mapCenter.lat = -41.2942303
+      mapCenter.lng = 174.7958889
     }
 
     let logoLocation = {
-        lat: -41.2942303,
-        lng: 174.7958889
-    } 
+      lat: -41.2942303,
+      lng: 174.7958889
+    }
 
-  
     let map = new google.maps.Map(document.getElementById('map'), {
-        center: mapCenter,
-        zoom: 14
-    });
+      center: mapCenter,
+      zoom: 14
+    })
 
     let marker = new google.maps.Marker({
-        position: logoLocation,
-        map: map,
-        
-    });
-    
+      position: logoLocation,
+      map: map
+
+    })
+  }
+
+  (this.events = () => {
+    const here = this
+    document.addEventListener('DOMContentLoaded', this.initMap)
+    window.addEventListener('resize', function resizeMap (e) {
+      
+      let newWidth = e.target.innerWidth
+      console.log(newWidth)
+      console.log(oldWidth)
+      if ((newWidth >= resize && oldWidth < resize) || (newWidth <= resize && oldWidth >= resize )) {
+        here.initMap()
+        oldWidth = newWidth
+      }
+    })
+  })()
 }
 
-
-
-
+export default Map
